977d9c8796ae8aa89bd302856ce2f396bab58dac
Anthony Serravalle <anthony.serravalle@gmail.com>
2017-07-30 17:58:48 -0400

    Some initial refactoring to deal with synchronization issues

--- src/main/java/br/edu/utfpr/universidade/AlunoBean.java
+++ src/main/java/br/edu/utfpr/universidade/AlunoBean.java
@@ -2,9 +2,9 @@ package br.edu.utfpr.universidade;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Random;
-import javax.faces.bean.ManagedBean;
+
 import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 @ManagedBean
@@ -23,30 +23,22 @@ public class AlunoBean implements Serializable {
     }
 
     public void atualizaListaAlunos() {
-        alunos = EManager.getInstance().createNamedQuery("Aluno.findAll").getResultList();
+        alunos = EManager.getAlunos();
     }
 
     public void modificaAluno() {
-        EManager.getInstance().getTransaction().begin();
-        EManager.getInstance().merge(this.alunoSelecionado);
-        EManager.getInstance().getTransaction().commit();
+        EManager.modificaAluno(this.alunoSelecionado);
         atualizaListaAlunos();
     }
 
     public void deletaAluno() {
-        List<Matricula> m = EManager.getInstance().createNamedQuery("Matricula.findByAluno").setParameter("idAluno", this.alunoSelecionado.getId()).getResultList();
-        EManager.getInstance().getTransaction().begin();
-        for (int i = 0; i < m.size(); i++) {
-            EManager.getInstance().remove(m.get(i));
-        }
-        EManager.getInstance().remove(this.alunoSelecionado);
-        EManager.getInstance().getTransaction().commit();
+        EManager.deletaAluno(this.alunoSelecionado);
         atualizaListaAlunos();
     }
 
     public void enviaAluno(Aluno a) {
         this.alunoSelecionado = a;
-        List<Matricula> m = EManager.getInstance().createNamedQuery("Matricula.findByAluno").setParameter("idAluno", this.alunoSelecionado.getId()).getResultList();
+        List<Matricula> m = EManager.getMatriculas(a);
         if (m.size() > 0) {
             this.msgConfirmacao = "Tem certeza? A remoção do(a) aluno(a) acarretará na exclusão de todas as respectivas matrículas.";
             this.larguraPopupConfirma = 400;
@@ -57,11 +49,7 @@ public class AlunoBean implements Serializable {
     }
 
     public void novoCadastro() {
-        this.aluno.setMatricula(1000000 + new Random().nextInt(9999999 - 1000000 + 1));
-        EManager.getInstance().getTransaction().begin();
-        EManager.getInstance().persist(this.aluno);
-        EManager.getInstance().getTransaction().commit();
-        this.aluno = new Aluno();
+        EManager.insereAluno(aluno);
         atualizaListaAlunos();
     }
 
--- src/main/java/br/edu/utfpr/universidade/EManager.java
+++ src/main/java/br/edu/utfpr/universidade/EManager.java
@@ -1,22 +1,84 @@
 package br.edu.utfpr.universidade;
 
+import java.io.Serializable;
+import java.util.List;
+import java.util.Random;
+
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class EManager implements java.io.Serializable{
+public class EManager implements Serializable {
 
-    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("UniversidadePU");
-    private static EntityManager em = emf.createEntityManager();
+    private static final long serialVersionUID = 3675946111000695658L;
 
-    public EManager() {
+    /* Mutex for ensuring the "EManager" singleton is created only once. */
+    private static final Object emLock = new Object();
+    private static EntityManager em = null;
+
+    /* Mutex for ensuring mutual exclusion of access to the database. */
+    private static final Object operationLock = new Object();
+
+    private EManager() {
     }
 
     public static EntityManager getInstance() {
         if (em == null) {
-            em = emf.createEntityManager();
+            synchronized (emLock) {
+                if (em == null) {
+                    em = Persistence.createEntityManagerFactory("UniversidadePU").createEntityManager();
+                }
+            }
         }
         return em;
+    }
+
+    public static List<Aluno> getAlunos() {
+        synchronized (operationLock) {
+            return getInstance().createNamedQuery("Aluno.findAll").getResultList();
+        }
+    }
+
+    public static void modificaAluno(Aluno aluno) {
+        synchronized (operationLock) {
+            getInstance().getTransaction().begin();
+            getInstance().merge(aluno);
+            getInstance().getTransaction().commit();
+        }
+    }
+
+    public static void deletaAluno(Aluno aluno) {
+        synchronized (operationLock) {
+            List<Matricula> m = getMatriculas(aluno);
+            getInstance().getTransaction().begin();
+            for (int i = 0; i < m.size(); i++) {
+                EManager.getInstance().remove(m.get(i));
+            }
+            EManager.getInstance().remove(aluno);
+            EManager.getInstance().getTransaction().commit();
+        }
+    }
+
+    /**
+     * Returns the matricula associated with selected aluno
+     *
+     * @param aluno
+     *
+     */
+    public static List<Matricula> getMatriculas(Aluno aluno) {
+        synchronized (operationLock) {
+            return getInstance().createNamedQuery("Matricula.findByAluno").setParameter("idAluno", aluno.getId())
+                    .getResultList();
+        }
+    }
+
+    public static void insereAluno(Aluno aluno) {
+        synchronized (operationLock) {
+            aluno.setMatricula(1000000 + new Random().nextInt(9999999 - 1000000 + 1));
+            EManager.getInstance().getTransaction().begin();
+            EManager.getInstance().persist(aluno);
+            EManager.getInstance().getTransaction().commit();
+            aluno = new Aluno();
+        }
     }
 
 }
